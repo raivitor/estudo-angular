f244a2943d62e8b60e40e6d43d0bec422c5c748b
Rai Vitor <raivitor@gmail.com>
2019-09-30 15:16:01 -0300

    create module.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,12 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppComponent } from './app.component';
-import { PhotoComponent } from './photo/photo.component';
+import { AppComponent } from "./app.component";
+import { PhotosModule } from "./photos/photos.module";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    PhotoComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, PhotosModule],
   providers: [],
   bootstrap: [AppComponent]
 })
